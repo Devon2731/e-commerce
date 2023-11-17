94c75fe24b2b1732a8e730dade5b9dbd7a6c5375
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-11-17 13:22:01 -0500

    modifying the models

--- models/index.js
+++ models/index.js
@@ -7,17 +7,20 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 
@@ -25,18 +28,16 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
+  unique: false,
+  onDelete: 'CASCADE',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
-});
-
-// Tags belongToMany Products (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
+  unique: false,
+  onDelete: 'CASCADE',
 });
 
 module.exports = {
